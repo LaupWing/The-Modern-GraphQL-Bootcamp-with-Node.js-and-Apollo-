6cd9d9b128a931304df1b472a7e0794f5cfd1170
LaupWing <loc_strict@hotmail.com>
2021-04-17 12:43:30 +0200

    35

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,10 +1,13 @@
-import { GraphQLServer } from 'graphql-yoga'
+import { GraphQLServer, PubSub } from 'graphql-yoga'
 import db from './db'
 import Query from './resolvers/Query'
 import Comment from './resolvers/Comment'
 import User from './resolvers/User'
 import Mutation from './resolvers/Mutation'
+import Subscription from './resolvers/Subscription'
 import Post from './resolvers/Post'
+
+const pubsub = new PubSub()
 
 // Resolvers
 const resolvers = {
@@ -12,14 +15,16 @@ const resolvers = {
    Mutation,
    Comment,
    User,
-   Post
+   Post,
+   Subscription
 }
 
 const server = new GraphQLServer({
    typeDefs: './src/schema.graphql',
    resolvers,
    context:{
-      db
+      db,
+      pubsub
    }
 })
 
