9f9b3c116cbb27af22bf65d47ed56d94009dcf70
LaupWing <loc_strict@hotmail.com>
2021-05-03 17:35:50 +0200

    53

--- graphql-prisma/src/prisma.js
+++ graphql-prisma/src/prisma.js
@@ -5,6 +5,47 @@ const prisma = new Prisma({
    endpoint: 'http://localhost:4466'
 })
 
+const createPostForUse = async (authorId, data)=>{
+   await prisma.mutation.createPost({
+      data:{
+         ...data,
+         author:{
+            connect:{
+               id: authorId
+            }
+         }
+      }
+   }, '{id}')
+
+   const user = await prisma.query.user({
+      where:{
+         id: authorId
+      }
+   }, '{id name email posts {id title published}}')
+   return user
+}
+
+const updatePostForUser = async (id, update)=>{
+   const post = await prisma.mutation.updatePost({
+      where:{
+         id
+      },
+      data:{...update}
+   }, '{author {id}}')
+
+   const user = await prisma.query.user({
+      where:{
+         id: post.author.id
+      }
+   }, '{id name email posts {id title published}}')
+}
+
+// createPostForUse('cknyl0dlk001b0a95fzum8td3', {
+//    body: 'test',
+//    title: 'test',
+//    published: true
+// }).then(user=>console.log(JSON.stringify(user, undefined, 2)))
+
 // prisma.query.users(null, '{ id name}')
 //    .then(user=>console.log(user))
 
