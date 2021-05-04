9fddcd4949635b3b4446e9f4f4ae0e23b2f8efbc
LaupWing <loc_strict@hotmail.com>
2021-05-04 14:21:19 +0200

    59

--- graphql-prisma/src/index.js
+++ graphql-prisma/src/index.js
@@ -6,7 +6,7 @@ import User from './resolvers/User'
 import Mutation from './resolvers/Mutation'
 import Subscription from './resolvers/Subscription'
 import Post from './resolvers/Post'
-import './prisma'
+import prisma from './prisma'
 
 const pubsub = new PubSub()
 
@@ -25,7 +25,8 @@ const server = new GraphQLServer({
    resolvers,
    context:{
       db,
-      pubsub
+      pubsub,
+      prisma
    }
 })
 
--- graphql-prisma/src/resolvers/Query.js
+++ graphql-prisma/src/resolvers/Query.js
@@ -1,15 +1,17 @@
 const Query = {
-   users(parent, args, {db} , info){
-      if(!args.query){
-         return db.users
-      }
-      return db.users.filter(user=>user.name.toLocaleLowerCase().includes(args.query.toLocaleLowerCase()))
-   },
-   posts(parent, args, {db} , info){
-      if(!args.query){
-         return db.posts
-      }
-      return db.posts.filter(post=>post.body.toLocaleLowerCase().includes(args.query.toLocaleLowerCase()) || post.title.toLocaleLowerCase().includes(args.query.toLocaleLowerCase()))
+   users(parent, args, {db, prisma} , info){
+      return prisma.query.users(null, info)
+      // if(!args.query){
+         //    return db.users
+         // }
+         // return db.users.filter(user=>user.name.toLocaleLowerCase().includes(args.query.toLocaleLowerCase()))
+      },
+   posts(parent, args, {db, prisma} , info){
+      return prisma.query.posts(null, info)
+      // if(!args.query){
+      //    return db.posts
+      // }
+      // return db.posts.filter(post=>post.body.toLocaleLowerCase().includes(args.query.toLocaleLowerCase()) || post.title.toLocaleLowerCase().includes(args.query.toLocaleLowerCase()))
    },
    me(){
       return users[0]
