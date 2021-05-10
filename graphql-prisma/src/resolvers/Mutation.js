f5423adf9ef03d493bbb69260df4dcc660e76a98
LaupWing <loc_strict@hotmail.com>
2021-05-10 17:46:25 +0200

    70

--- graphql-prisma/src/resolvers/Mutation.js
+++ graphql-prisma/src/resolvers/Mutation.js
@@ -13,11 +13,27 @@ const Mutation = {
             ...args.data,
             password: hashed
          }
-      }, info)
+      })
 
       return {
          user,
          token: jwt.sign({userId: user.id}, 'thisisasecret')
+      }
+   },
+   async login(parent, args, {prisma}, info){
+      const user = await prisma.query.user({
+         where: {
+            email: args.data.email
+         }
+      })
+
+      if(!user){
+         throw new Error('Unable to login')
+      }
+      const isMatch = await bcrypt.compare(args.data.password, user.password)
+
+      if(!isMatch){
+         throw new Error('Unable to login')
       }
    },
    async deleteUser(parent, args, {prisma}, info){
